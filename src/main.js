d32397f162cf5bbbd2840e9a48c0e3f1cde56131
c26ab1 <c26ab1@dalton.org>
2024-07-24 11:56:16 -0400

    routing fix 4

--- src/main.js
+++ src/main.js
@@ -1,23 +1,13 @@
 import './app.css'
-import App from './App.svelte'
-import App2 from './App2.svelte'
+import App from './App.svelte';
 import { inject } from '@vercel/analytics';
  
 inject({ mode: 'production' });
 
-let url_object = new URL(window.location.href);
-let url = url_object.href;
 
-console.log(url);
+const app = new App({
+  target: document.getElementById('app')
+});
 
-let app = new App({
-  target: document.getElementById('app'),
-})
-
-if (url.includes("theme?cleantechies")) {
-  app = new App2({
-    target: document.getElementById('app'),
-  })
-}
 
 export default app
